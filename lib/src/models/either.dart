0357cb34e37d2cb1967f35f622c3d9fc13cc9bff
Shuvo prosad sarnakar <shuvoprosad@gmail.com>
2025-12-22 22:51:46 +0600

    Refactor `Either` class
    - Update `Left` and `Right` constructors to use super-parameter initialization.
    - Replace strict runtime type checks (`data.runtimeType == Type`) with `is` type checks in `getOrElse`, `swap`, and `fold` methods to support subtypes correctly.

--- lib/src/models/either.dart
+++ lib/src/models/either.dart
@@ -1,31 +1,27 @@
 part of 'models.dart';
 
 class Left<L, R> extends Either<L, R> {
-  Left(L data) : super._(data);
+  Left(L super.data) : super._();
 }
 
 class Right<L, R> extends Either<L, R> {
-  Right(R data) : super._(data);
+  Right(R super.data) : super._();
 }
 
 class Either<L, R> {
   final dynamic data;
-
   Either._(this.data);
-
   R getOrElse(R Function() callback) {
-    if (data.runtimeType == R) {
+    if (data is R) {
       return data as R;
     }
     return callback();
   }
 
   bool isLeft() => this is Left;
-
   bool isRight() => this is Right;
-
   Either<R, L> swap() {
-    if (data.runtimeType == L) {
+    if (data is L) {
       return Right((data as L));
     }
     return Left((data as R));
@@ -36,7 +32,7 @@ class Either<L, R> {
     FutureOr<T> Function(R) onRight,
   ) {
     log('data type ${data.runtimeType}');
-    if (data.runtimeType == L) {
+    if (data is L) {
       return onLeft((data as L));
     }
     return onRight((data as R));
